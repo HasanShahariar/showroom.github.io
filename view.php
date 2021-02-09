c8d09be5de0147c3e7f1acb9ca3eb55fb6769a10
HasanShahariar <78812668+HasanShahariar@users.noreply.github.com>
2021-02-10 00:20:19 +0600

    Showroom

---
+++ view.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="jumbotron">
+            <div class="card text-center">
+                <div class="card-header">
+                    
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <table class="table table-striped table-dark">
+                        <thead>
+                            <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Product Name</th>
+                            <th scope="col">Product Brand</th>
+                            <th scope="col">Parchasing Price</th>
+                            <th scope="col">Selling Price</th>
+                            <th scope="col">Availability</th>
+                            <th scope="col">Total Price</th>
+                            <th scope="col">Action</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td>001</td>
+                            <td>TV</td>
+                            <td>Walton</td>
+                            <td>20000</td>
+                            <td>25500</td>
+                            <td>6</td>
+                            <td>120000</td>
+                            <td>
+                            <a href="test.html">
+                            <button action="/test.html" class="btn btn-primary" style="width:80px;">Edit</button>
+                            </a>
+                            <button class="btn btn-danger" style="width:80px;">Delete</button>
+                            </td>
+
+                            </tr>
+                        </tbody>
+                        </table>
+                </div>
+                <div class="card-footer text-muted">
+                    2 days ago
+                </div>
+                </div>
+
+            </div>
+    </div>
+</body>
+</html>
